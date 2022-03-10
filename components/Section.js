a51cbb1d68351b2749ef3b49e3468c451460386e
shyam205 <mastershyam2013@gmail.com>
2022-03-10 13:20:35 +0530

    added some style

--- components/Section.js
+++ components/Section.js
@@ -1,17 +1,20 @@
 import Link from 'next/link'
 import Card from './Card';
+
 const Section = ({genre, videos}) => {
- 
+    
     return(
         <div className='section'>
             <h3>{genre}</h3>
+            <div className='video-feed-section-container'>
             <div className='video-feed-section'>
-             {videos?.map(video => (
-                 <Link key={video.id} href={`/video/${video.slug}`}>
-                     <Card video={video} />
-                 </Link>
-                 
-             ))}
+                    {videos?.map(video => (
+                        <Link key={video.id} href={`/video/${video.slug}`}>
+                            <Card video={video} />
+                        </Link>
+                        
+                    ))}
+                </div>
             </div>
         </div>
     )
